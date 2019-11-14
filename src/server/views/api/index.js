fd92027c55aeb09ee86278743a39b1072f08e00f
John Donovan <john@asktia.com>
2019-11-14 12:14:58 -0800

    stashing incomplete work to promote delayed jobs

--- src/server/views/api/index.js
+++ src/server/views/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
 const jobAdd = require('./jobAdd');
-const jobRetry = require('./jobRetry');
+//const jobRetry = require('./jobRetry');
+const jobPromote = require('./jobPromote')
 const jobRemove = require('./jobRemove');
 const bulkJobsRemove = require('./bulkJobsRemove');
 const bulkJobsRetry = require('./bulkJobsRetry');
@@ -9,7 +10,7 @@ const bulkJobsRetry = require('./bulkJobsRetry');
 router.post('/queue/:queueHost/:queueName/job', jobAdd);
 router.post('/queue/:queueHost/:queueName/job/bulk', bulkJobsRemove);
 router.patch('/queue/:queueHost/:queueName/job/bulk', bulkJobsRetry);
-router.patch('/queue/:queueHost/:queueName/job/:id', jobRetry);
+router.patch('/queue/:queueHost/:queueName/job/:id', jobPromote);
 router.delete('/queue/:queueHost/:queueName/job/:id', jobRemove);
 
 module.exports = router;
